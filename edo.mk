2ce4aa13fe2f02beb70740e793c145961a31688a
IQUXAe <iquxae@example.com>
2025-12-15 22:15:17 +0300

    Revert "sm8250-common: Enable Smooth Display settings via RRO option"
    This reverts commit 8d78ca8f199d837983333956d915e0af507da437.

--- edo.mk
+++ edo.mk
@@ -44,7 +44,6 @@ PRODUCT_PACKAGES += \
     SimSingleResCommon \
     SonyEdoFrameworksResCommon \
     SonyEdoSettingsProviderOverlayCommon \
-    SonyEdoSettingsRes \
     SonyEdoSettingsResCommon \
     SonyEdoSystemUIResCommon \
     SonyEdoTelephonyResCommon \
